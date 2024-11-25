c28789da798d75c48dbfa6664ccadb055eeb7e7c
Ritchie Yu <ritchie.yu@mail.mcgill.ca>
2024-11-25 14:57:20 -0500

    make dataloaders compatible with maisi transformation pipeline

--- generation/maisi/dataset.py
+++ generation/maisi/dataset.py
@@ -1,8 +1,10 @@
-from monai import transforms
+import torch
 from torch.utils.data import Dataset, DataLoader
-import numpy as np
+from monai import transforms
+from monai.transforms import convert_to_contiguous
 
 import os
+import numpy as np
 from typing import Union, List, Tuple
 
 from pvg.constants.loris.general import Sequences
@@ -10,7 +12,7 @@ from pvg.constants.loris.indexing import SubjectKeys as SJ_KEYS
 from pvg.constants.pipeline import SplitKeys
 from pvg.runner.dataset.dataset import get_leaf_val_from_dict, glob_file, load_image
 
-from utils import Hyperparameters, Compose
+from utils import MAISI_Transform
 
 
 # TODO: Modify the transforms used for training
@@ -25,31 +27,28 @@ class DatasetDescription:
 
         If the train/val/test dataset is a list/tuple of Dataset objects, the custom
         DataLoader will be used instead.
+
+        CHANGES:
+        - Transform must be defined outside of this class.
     """
-    def __init__(self):
+    def __init__(self, sequences):
         self.train_dataset = None
         self.val_dataset = None
         self.test_dataset = None
+        self.sequences = sequences
 
-    def setup_datasets(self, data_split, rootdir_dict):
+    def setup_datasets(self, data_split, rootdir_dict, train_transform, val_transform, test_transform):
         ################## MODIFY HERE #######################
         item_template = {
-                'MRI': [(SJ_KEYS.MRI_AND_LABEL, s) for s in Hyperparameters.sequences],
+                'MRI': [(SJ_KEYS.MRI_AND_LABEL, s) for s in self.sequences],
                 'MASK': [(SJ_KEYS.MRI_AND_LABEL, Sequences.BEAST)],
         }
         self.train_dataset = CustomVolumeLoader(data_split[SplitKeys.TRAIN], rootdir_dict, item_template,
-                transform=Compose([
-                    transforms.SpatialPadd(**Hyperparameters.transforms['model']['SpatialPadd']),
-                    # transforms.RandFlipd(**Hyperparameters.transforms['model']['RandFlipd']),  # TODO: Do all transforms get applied to the mask as well?
-                    ]))
+                transform=train_transform)
         self.val_dataset = CustomVolumeLoader(data_split[SplitKeys.VALIDATION], rootdir_dict, item_template,
-                transform=Compose([
-                    transforms.SpatialPadd(**Hyperparameters.transforms['model']['SpatialPadd'])
-                    ]))
+                transform=val_transform)
         self.test_dataset = CustomVolumeLoader(data_split[SplitKeys.TEST], rootdir_dict, item_template,
-                transform=Compose([
-                    transforms.SpatialPadd(**Hyperparameters.transforms['model']['SpatialPadd'])
-                    ]))
+                transform=test_transform)
         ######################################################
 
     def get_train_dataset(self) -> Union[Dataset, List[Dataset], Tuple[Dataset]]:
@@ -61,7 +60,7 @@ class DatasetDescription:
     def get_test_dataset(self) -> Union[Dataset, List[Dataset], Tuple[Dataset]]:
         return [self.test_dataset]
     
-class CustomVolumeLoader(Dataset):
+class CustomVolumeLoader(Dataset):  # dataset class
     def __init__(self, dataset_dict, rootdir_dict, item_template, transform=None):
         """
             dataset_dict: dict with format of { sample_key: sample_dict }
@@ -107,6 +106,14 @@ class CustomVolumeLoader(Dataset):
             imgs = [load_image(path) for path in img_paths]
             output[item_key] = np.stack(imgs)
 
+        # Perform skull-stripping
+        output["MRI"] = output["MRI"] * output["MASK"]
+        output["MRI"] = torch.from_numpy(output["MRI"]).contiguous()  # TODO: Why does this not fix the error message?
+        del output["MASK"]  # not needed anymore
+        
+        # output = convert_to_contiguous(output, memory_format=torch.contiguous_format)  # memory_format argument applies if input is a torch tensor
+
+        # Run transformation pipeline
         if self.transform is not None:
             output = self.transform(output)
 
@@ -127,35 +134,126 @@ class CustomDataLoader:
         - modified distributed loader to classic loader
         - changed 'dataset[0]' to 'dataset'
     """
+    def __init__(self, train_batch_size, val_batch_size, test_batch_size, num_workers):
+        self.train_batch_size = train_batch_size
+        self.val_batch_size = val_batch_size
+        self.test_batch_size = test_batch_size
+        self.num_workers = num_workers
 
-    @staticmethod
-    def get_train_loader(dataset):
+    def get_train_loader(self, dataset):
         dataloader = DataLoader( 
             dataset,
-            batch_size=Hyperparameters.batch_size,
+            batch_size=self.train_batch_size,
             drop_last=True,
-            num_workers=Hyperparameters.num_workers,
+            num_workers=self.num_workers,
             pin_memory=True,
             persistent_workers=True)
         return dataloader
 
-    @staticmethod
-    def get_val_loader(dataset):
+    def get_val_loader(self, dataset):
         dataloader = DataLoader(
             dataset,
-            batch_size=Hyperparameters.batch_size,
+            batch_size=self.val_batch_size,
             drop_last=True,
-            num_workers=Hyperparameters.num_workers,
+            num_workers=self.num_workers,
             pin_memory=True,
             persistent_workers=True)
         return dataloader
 
-    @staticmethod
-    def get_test_loader(dataset):
+    def get_test_loader(self, dataset):
         dataloader = DataLoader(
             dataset,
-            batch_size=Hyperparameters.batch_size,
+            batch_size=self.test_batch_size,
             drop_last=True,
-            num_workers=Hyperparameters.num_workers,
+            num_workers=self.num_workers,
             pin_memory=True)
         return dataloader
+    
+if __name__ == '__main__':
+    import torch
+    import json
+    import argparse
+    import matplotlib.pyplot as plt
+    from pvg.runner.utilities.io import IO
+    from utils import define_train_transform, define_val_transform
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-dir', required=True, help='Parent directory of LORIS dataset (SLURM_TMPDIR)')
+    pargs = parser.parse_args()
+
+    data_dir = pargs.data_dir
+
+    args = argparse.Namespace()
+
+    # Load environment
+    environment_file = "./configs/environment_maisi_vae_train.json"
+    env_dict = json.load(open(environment_file, "r"))
+    for k, v in env_dict.items():
+        setattr(args, k, v)
+        print(f"{k}: {v}")
+
+    # Load configuration files
+    config_file = "./configs/config_maisi.json"
+    config_dict = json.load(open(config_file, "r"))
+    for k, v in config_dict.items():
+        setattr(args, k, v)
+
+    config_train_file = "./configs/config_maisi_vae_train.json"
+    config_train_dict = json.load(open(config_train_file, "r"))
+    for k, v in config_train_dict["data_option"].items():
+        setattr(args, k, v)
+        print(f"{k}: {v}")
+    for k, v in config_train_dict["autoencoder_train"].items():
+        setattr(args, k, v)
+        print(f"{k}: {v}")
+
+    
+    train_transform = define_train_transform(random_aug=args.random_aug,
+                                             patch_size=args.patch_size,
+                                             output_dtype=torch.float16,  # final data type
+                                             spacing_type=args.spacing_type,
+                                             spacing=args.spacing,
+                                             image_keys=["MRI"],
+                                             label_keys=[],
+                                             additional_keys=[],
+                                             select_channel=0)
+
+    
+    val_transform = define_val_transform(k=4,  # patches should be divisible by k
+                                         val_patch_size=args.val_patch_size,  # if None, will validate on whole image volume
+                                         output_dtype=torch.float16,  # final data type
+                                         image_keys=["MRI"],
+                                         label_keys=[],
+                                         additional_keys=[],
+                                         select_channel=0)
+    
+    test_transform = val_transform
+
+    splits_path = '/home/ritchiey/projects/def-arbeltal/ritchiey/capstone/old_splits.pickle'  # using 1x1x3_v4_0_w000 for now...
+    rootdir_dict = {"MRI_AND_LABEL": f"{data_dir}/loris_1x1x3_v4_0_w000"}
+    splits = IO.load_pickle(splits_path)
+    dataset_descriptor = DatasetDescription(sequences=[Sequences.FLR])
+    dataset_descriptor.setup_datasets(data_split=splits["experiment"],
+                                      rootdir_dict=rootdir_dict,
+                                      train_transform=MAISI_Transform(train_transform),
+                                      val_transform=MAISI_Transform(val_transform),
+                                      test_transform=MAISI_Transform(test_transform))
+
+    dataloader = CustomDataLoader(train_batch_size=32,
+                              val_batch_size=1,
+                              test_batch_size=1,
+                              num_workers=7)
+    
+    dataloader_train = dataloader.get_train_loader(dataset_descriptor.train_dataset)
+    dataloader_val = dataloader.get_val_loader(dataset_descriptor.val_dataset)
+    dataloader_test = dataloader.get_test_loader(dataset_descriptor.test_dataset)
+
+    # for _ in dataloader_train:
+        # continue
+
+    sample = next(iter(dataloader_val))
+    print(sample["MRI"].shape)
+
+    # fig, ax = plt.subplots()
+    # ax.imshow(sample['MRI'][0, 0, 32, :, :], cmap='gray')
+    # fig.savefig("sample_transformed.png", dpi=500)
